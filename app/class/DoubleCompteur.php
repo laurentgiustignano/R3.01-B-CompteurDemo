4e0e4c1fa85ac7a6ecae95e9a6e2e4fb9ac55e6a
Laurent Giustignano <laurent.giustignano@free.fr>
2024-11-19 11:16:39 +0100

    Double Compteur avec parent::

--- app/class/DoubleCompteur.php
+++ app/class/DoubleCompteur.php
@@ -5,9 +5,6 @@ require_once 'Compteur.php';
 readonly class DoubleCompteur extends Compteur {
 
   public function nombreVues(): int {
-    if(!file_exists($this->filename)) {
-      return 0;
-    }
-    return 2 * (int) file_get_contents($this->filename);
+    return parent::nombreVues() * 2;
   }
 }
